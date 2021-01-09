2df24297c5ba8d3b6ec8fa6851d14314940bcf01
Rodrigo Lira <8973745+rodrigolira@users.noreply.github.com>
2021-01-09 00:50:26 -0300

    add JSDoc annotations

--- src/components/traffic-light/traffic-light.tsx
+++ src/components/traffic-light/traffic-light.tsx
@@ -9,8 +9,19 @@ import { TrafficLightMode } from './traffic-light-mode';
   shadow: true,
 })
 export class TrafficLight {
+  /** Specifies whether lights should be turned on or off. It defaults to `off`.
+   * 
+   *  Setting it to `on` will turn on the light specified in the `color` property.
+   * 
+   *  Setting it to `all-on` will turn on all the lights when `mode` is set to `three-lights`.
+   *  When in `single-light` mode, setting the `current-state` to `all-on` has the same effect as setting it to `on` (turning on the single light).
+   */
   @Prop({ reflect: true }) currentState: TrafficLightState = TrafficLightState.Off;
+  
+  /** The color of the light that should be turned on. This is ignored when `current-state` is off or when in `three-lights` mode and with `current-state` set to `all-on`. */
   @Prop({ reflect: true }) color: TrafficLightColor;
+
+  /** Mode in which the component is displayed. It defaults to `three-lights-mode`. */
   @Prop({ reflect: true }) mode: TrafficLightMode;   // will default to TrafficLightMode.ThreeLights
 
   @Watch('currentState')
