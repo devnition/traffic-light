da403bb19fd8462e2e0946b4f88347141376490d
Rodrigo Lira <8973745+rodrigolira@users.noreply.github.com>
2021-01-09 00:51:29 -0300

    update documentation and definitions generated

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,8 +10,17 @@ import { TrafficLightColor } from "./components/traffic-light/traffic-light-colo
 import { TrafficLightMode } from "./components/traffic-light/traffic-light-mode";
 export namespace Components {
     interface TrafficLight {
+        /**
+          * The color of the light that should be turned on. This is ignored when `current-state` is off or when in `three-lights` mode and with `current-state` set to `all-on`.
+         */
         "color": TrafficLightColor;
+        /**
+          * Specifies whether lights should be turned on or off. It defaults to `off`.  Setting it to `on` will turn on the light specified in the `color` property.  Setting it to `all-on` will turn on all the lights when `mode` is set to `three-lights`. When in `single-light` mode, setting the `current-state` to `all-on` has the same effect as setting it to `on` (turning on the single light).
+         */
         "currentState": TrafficLightState;
+        /**
+          * Mode in which the component is displayed. It defaults to `three-lights-mode`.
+         */
         "mode": TrafficLightMode;
     }
 }
@@ -28,8 +37,17 @@ declare global {
 }
 declare namespace LocalJSX {
     interface TrafficLight {
+        /**
+          * The color of the light that should be turned on. This is ignored when `current-state` is off or when in `three-lights` mode and with `current-state` set to `all-on`.
+         */
         "color"?: TrafficLightColor;
+        /**
+          * Specifies whether lights should be turned on or off. It defaults to `off`.  Setting it to `on` will turn on the light specified in the `color` property.  Setting it to `all-on` will turn on all the lights when `mode` is set to `three-lights`. When in `single-light` mode, setting the `current-state` to `all-on` has the same effect as setting it to `on` (turning on the single light).
+         */
         "currentState"?: TrafficLightState;
+        /**
+          * Mode in which the component is displayed. It defaults to `three-lights-mode`.
+         */
         "mode"?: TrafficLightMode;
     }
     interface IntrinsicElements {
